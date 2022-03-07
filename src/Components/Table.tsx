781b1b1a38034d3f6308f8a1ee1a2e56335f1ba5
Eduardo Miranda <42817510+edumi538@users.noreply.github.com>
2022-03-07 14:45:13 -0300

    Adicionado coluna type(Faltante)

--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -10,8 +10,12 @@ export default function DataTable() {
       .then((data) => {
         const newArrayObject = data.map((item, indice) => ({
           id: indice + 1,
-          ...item,
-          Initial: item.Initial || "INICIAL NÃO ENCONTRADA"
+          Name: item.Name || "NOME NÃO ENCONTRADO",
+          Initial: item.Initial || "INICIAL NÃO ENCONTRADA",
+          Region: item.Region || "REGIÃO NÃO ENCONTRADA",
+          State: item.State || "ESTADO NÃO ENCONTRADO",
+          RegionType: item.RegionType || "TIPO DE REGIÃO NÃO ENCONTRADO",
+          Type: item.Type || "TIPO NÃO ENCONTRADO"
         }));
         setData(newArrayObject);
       })
@@ -57,6 +61,13 @@ export default function DataTable() {
       field: "RegionType",
       align: "center",
       headerName: "RegionType",
+      headerAlign: "center",
+      width: 350
+    },
+    {
+      field: "Type",
+      align: "center",
+      headerName: "Type",
       headerAlign: "center",
       width: 350
     }
